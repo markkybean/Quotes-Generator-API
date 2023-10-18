e5316161a467b46817bc037b1497c2dd5bbf4792
markkybean <marksantos320@gmail.com>
2023-10-19 01:47:08 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// fetch("https://type.fit/api/quotes")
+// .then( random => random.json() )
+// .then( quotes =>{
+//     quotes.forEach(msg => {
+//         document.querySelector("#quotes").innerHTML= `<p>${msg.text}</p>'
+//     })
+// })
+document.getElementById("btnQuotes").addEventListener("click", function() {
+    fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(quotes => {
+        // Get a random quote from the array
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        const randomQuote = quotes[randomIndex].text;
+
+        // Display the random quote in the "quotes" element
+        document.querySelector("#quotes").innerHTML = `<h4>"${randomQuote}"</h4>`;
+    });
+});
